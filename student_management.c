306af39d79689f6aab8049250cae2c109af14277
Poojapm123 <poojapmmahadevappa@gmail.com>
2026-07-20 09:05:39 +0530

    Update student_management.c

--- student_management.c
+++ student_management.c
@@ -1,1 +1,114 @@
 student_management.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student {
+    int id;
+    char name[50];
+    float marks;
+};
+
+void addStudent() {
+    FILE *fp = fopen("students.txt", "a");
+    struct student s;
+
+    printf("Enter ID: ");
+    scanf("%d", &s.id);
+    printf("Enter Name: ");
+    scanf(" %[^\n]", s.name);
+    printf("Enter Marks: ");
+    scanf("%f", &s.marks);
+
+    fwrite(&s, sizeof(s), 1, fp);
+    fclose(fp);
+
+    printf("Student Added Successfully!\n");
+}
+
+void displayStudents() {
+    FILE *fp = fopen("students.txt", "r");
+    struct student s;
+
+    printf("\n--- Student List ---\n");
+    while (fread(&s, sizeof(s), 1, fp)) {
+        printf("ID: %d | Name: %s | Marks: %.2f\n", s.id, s.name, s.marks);
+    }
+    fclose(fp);
+}
+
+void searchStudent() {
+    FILE *fp = fopen("students.txt", "r");
+    struct student s;
+    int id, found = 0;
+
+    printf("Enter ID to search: ");
+    scanf("%d", &id);
+
+    while (fread(&s, sizeof(s), 1, fp)) {
+        if (s.id == id) {
+            printf("Found: %s (Marks: %.2f)\n", s.name, s.marks);
+            found = 1;
+        }
+    }
+    if (!found)
+        printf("Student Not Found!\n");
+
+    fclose(fp);
+}
+
+void deleteStudent() {
+    FILE *fp = fopen("students.txt", "r");
+    FILE *temp = fopen("temp.txt", "w");
+    struct student s;
+    int id, found = 0;
+
+    printf("Enter ID to delete: ");
+    scanf("%d", &id);
+
+    while (fread(&s, sizeof(s), 1, fp)) {
+        if (s.id != id) {
+            fwrite(&s, sizeof(s), 1, temp);
+        } else {
+            found = 1;
+        }
+    }
+
+    fclose(fp);
+    fclose(temp);
+
+    remove("students.txt");
+    rename("temp.txt", "students.txt");
+
+    if (found)
+        printf("Student Deleted!\n");
+    else
+        printf("Student Not Found!\n");
+}
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\n--- Student Management ---\n");
+        printf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Search Student\n");
+        printf("4. Delete Student\n");
+        printf("5. Exit\n");
+
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: addStudent(); break;
+            case 2: displayStudents(); break;
+            case 3: searchStudent(); break;
+            case 4: deleteStudent(); break;
+            case 5: exit(0);
+            default: printf("Invalid choice!\n");
+        }
+    }
+
+    return 0;
+}
